df994e73afedbd58c10af184f849e92ad07ab8d7
Plasmoxy <sebohacker@gmail.com>
2018-02-13 00:25:45 +0100

    pretty good helloworld

--- dev/p5/helloworld/sketch.js
+++ dev/p5/helloworld/sketch.js
@@ -1,8 +1,30 @@
+var s = 10;
+var speed = 2;
+var direction = false;
+var minSize = 10, maxSize = 200;
+
 function setup() {
   createCanvas(windowWidth-15, windowHeight-20);
+  setInterval(function() {
+    if(direction) {
+      if (s<100)
+        s+=speed;
+    } else {
+      if (s>10)
+      s-=speed;
+    }
+  }, 1);
 }
 
 function draw() {
-  rect(30, 50, 100, 100);
-  background("#45c194");
+  background("#111111");
+  ellipse(mouseX,mouseY,s,s);
+}
+
+function mousePressed() {
+  direction = true;
+}
+
+function mouseReleased() {
+  direction = false;
 }
