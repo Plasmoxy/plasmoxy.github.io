525fd11710ae5d118506ace37900feb2d3cdc7ab
Plasmoxy <sebohacker@gmail.com>
2018-02-13 22:30:05 +0100

    sped

--- dev/p5/helloworld/sketch.js
+++ dev/p5/helloworld/sketch.js
@@ -1,5 +1,5 @@
 var s = 50;
-var speed = 1;
+var speed = 0;
 var direction = false;
 var minSize = 10, maxSize = 200;
 var wentFullScreen = false;
@@ -10,7 +10,7 @@ function update() {
       s+=speed;
   } else {
     if (s>minSize)
-    s-=speed;
+      s-=speed;
   }
 }
 
@@ -30,15 +30,24 @@ addEventListener("click", function() {
 });
 
 function setup() {
-  if (wentFullScreen) createCanvas(windowWidth-15, windowHeight-20);
+
+  if (wentFullScreen) {
+    createCanvas(windowWidth-15, windowHeight-20);
+    speed = windowWidth / 1000;
+  }
   else createCanvas(0,0);
+
   if (!wentFullScreen) setInterval(update, 1);
 }
 
 function draw() {
   background("#111111");
+
   fill("#00ffd9");
   ellipse(mouseX,mouseY,s,s);
+
+  textSize(15);
+  text("PRESS BACK/ESC TO EXIT", 30, 30);
 }
 
 function mousePressed() {
