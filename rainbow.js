05a86ea91c1f045253c7cfe1ef9e2427a2bc3616
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-02-19 08:39:54 +0100

    rainbow page

---
+++ rainbow.js
@@ -0,0 +1,11 @@
+var rainbow_hue = 0;
+var titleContainer = document.getElementById("titleContainer");
+
+setInterval(function() {
+  titleContainer.style.background = "hsl(" + rainbow_hue + ", 70%, 50%)";
+  if (rainbow_hue < 360) {
+    rainbow_hue++;
+  } else {
+    rainbow_hue = 0;
+  }
+}, 1);
