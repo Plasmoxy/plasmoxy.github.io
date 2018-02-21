901dadf440e684387ba29388b0a769f312152905
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-02-21 12:54:00 +0100

    LMAO SO GUD GRAPPH

--- dev/p5/functiongraph/sketch.js
+++ dev/p5/functiongraph/sketch.js
@@ -1,20 +1,23 @@
+var tu;
+
 var bounds = {
   width: 0,
   height: 0
 };
 
 var graphConf = {
-  minX: -10.0,
-  maxX: 10.0,
-  minY: -10.0,
-  maxY: 10.0,
+  minX: -5.0,
+  maxX: 5.0,
+  minY: -5.0,
+  maxY: 5.0,
   step: 0.001
 };
 
 var functions = [
   { f: function(x) {return x;}, clr: [255, 0, 0] },
-  { f: function(x) {return sin(4*x);}, clr: [0, 255, 0]},
-  { f: function(x) {return 1/x;}, clr: [0, 0, 255]}
+  { f: function(x) {return sin(x);}, clr: [0, 255, 0]},
+  { f: function(x) {return 1/x;}, clr: [0, 0, 255]},
+  { f: function(x) {return tan(4*x);}, clr: [0, 255, 255]}
 ];
 
 function relativeX(x) {
@@ -28,12 +31,14 @@ function relativeY(y) {
 function setup() {
   bounds.width = windowWidth - 15;
   bounds.height = windowHeight - 20;
+  tu = bounds.width / 64;
   createCanvas(bounds.width, bounds.height);
 
   background(0);
 
   // draw guide lines
   stroke(255, 255, 255);
+  fill(255, 255, 255);
 
   line(
     bounds.width/2, 0,
@@ -44,9 +49,23 @@ function setup() {
     bounds.width, bounds.height/2
   );
 
-
+  textSize(tu);
+  // draw number lines and stuff
   for (i = graphConf.minX; i<=graphConf.maxX; i++) {
+    line(
+      relativeX(i), bounds.height/2 - 5,
+      relativeX(i), bounds.height/2 + 5
+    );
+    text(""+i, relativeX(i) - 5, bounds.height/2 + 20)
 
+  }
+
+  for (i = graphConf.minY; i<=graphConf.maxY; i++) {
+    line(
+      bounds.width/2 - 5, relativeY(i),
+      bounds.width/2 + 5, relativeY(i)
+    );
+    text(""+i, bounds.width/2 - 15, relativeY(i) + 5);
   }
 
   // draw each function
