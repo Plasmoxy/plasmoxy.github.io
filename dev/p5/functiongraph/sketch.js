9fb0fcce1271c5a8338951163346cb27c2148fa0
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-02-21 11:29:05 +0100

    super good graph

--- dev/p5/functiongraph/sketch.js
+++ dev/p5/functiongraph/sketch.js
@@ -3,14 +3,27 @@ var bounds = {
   height: 0
 };
 
-var graphBounds = {
-  min: -1.0,
-  max: 1.0,
-  step: 0.1
+var graphConf = {
+  minX: -10.0,
+  maxX: 10.0,
+  minY: -10.0,
+  maxY: 10.0,
+  step: 0.001
 };
 
+var functions = [
+  { f: function(x) {return x;}, clr: [255, 0, 0] },
+  { f: function(x) {return sin(4*x);}, clr: [0, 255, 0]},
+  { f: function(x) {return 1/x;}, clr: [0, 0, 255]}
+];
 
-var f = function(x) {return x;};
+function relativeX(x) {
+  return map(x, graphConf.minX, graphConf.maxX, 0, bounds.width);
+}
+
+function relativeY(y) {
+  return bounds.height - map(y, graphConf.minY, graphConf.maxY, 0, bounds.height);
+}
 
 function setup() {
   bounds.width = windowWidth - 15;
@@ -18,11 +31,34 @@ function setup() {
   createCanvas(bounds.width, bounds.height);
 
   background(0);
-  stroke(255, 0, 0);
 
-  for (i = -1; i<=1; i++) {
-    point(i + 200, f(i) + 200);
+  // draw guide lines
+  stroke(255, 255, 255);
+
+  line(
+    bounds.width/2, 0,
+    bounds.width/2, bounds.height
+  );
+  line(
+    0, bounds.height/2,
+    bounds.width, bounds.height/2
+  );
+
+
+  for (i = graphConf.minX; i<=graphConf.maxX; i++) {
+
   }
+
+  // draw each function
+  functions.forEach(function(obj, index) {
+    stroke(obj.clr[0], obj.clr[1], obj.clr[2]);
+    for (x = graphConf.minX; x<=graphConf.maxX; x+=graphConf.step) {
+      point(
+        relativeX(x),
+        relativeY(obj.f(x))
+      );
+    }
+  });
 
 }
 
