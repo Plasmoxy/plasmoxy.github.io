98b7c7757255a13225d92a4fa98f9431b9f84f6b
Plasmoxy <sebohacker@gmail.com>
2018-02-27 18:12:59 +0100

    some useful stuff

--- dev/three.js/learning/helloworld/script.js
+++ dev/three.js/learning/helloworld/script.js
@@ -1,4 +1,4 @@
-console.log('< BEGINNING script.js >');
+console.log('< BEGIN script.js >');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -30,39 +30,70 @@ var keys = {
   backward: false
 };
 
+var canMove = {
+  up: true,
+  down: true,
+  left: true,
+  right: true,
+  forward: true,
+  backward: true
+};
 
-var roomGeometry = new THREE.BoxGeometry(2, 2, 2);
+
+var roomGeometry = new THREE.BoxGeometry(3, 3, 3);
 var roomMaterial = new THREE.MeshBasicMaterial({
   color: 0xFFFFFF,
   wireframe: true
 });
 var room = new THREE.Mesh(roomGeometry, roomMaterial);
 
-var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 var cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0x00ffc9,
   wireframe: true
 });
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-var geometry = new THREE.PlaneGeometry( 3, 3, 1);
+var geometry = new THREE.PlaneGeometry( 8, 8, 1);
 var material = new THREE.MeshBasicMaterial( {color: 0x555555, side: THREE.DoubleSide} );
 var plane = new THREE.Mesh( geometry, material );
 plane.rotation.x = Math.PI/2;
-plane.position.y = -1.01;
+plane.position.y = room.position.y - (new THREE.Box3().setFromObject( room )).getSize().y/2 - 0.01;
+
+var geo = new THREE.EdgesGeometry( roomGeometry );
+
+var mat = new THREE.LineBasicMaterial( { color: 0xFFFFFF, linewidth: 2 } );
+
+var wireframe = new THREE.LineSegments( geo, mat );
+
+scene.add( wireframe );
 
 scene.add(cube);
-scene.add(room);
 scene.add( plane );
 
 //render
 function update() {
-  if (keys.up) cube.position.y += 0.05;
-  if (keys.down) cube.position.y -= 0.05;
-  if (keys.right) cube.position.x += 0.05;
-  if (keys.left) cube.position.x -= 0.05;
-  if (keys.w) cube.position.z -= 0.05;
-  if (keys.s) cube.position.z += 0.05;
+  if (keys.up && canMove.up) cube.position.y += 0.05;
+  if (keys.down && canMove.down) cube.position.y -= 0.05;
+  if (keys.right && canMove.right) cube.position.x += 0.05;
+  if (keys.left && canMove.left) cube.position.x -= 0.05;
+  if (keys.forward && canMove.forward) cube.position.z -= 0.05;
+  if (keys.backward && canMove.backward) cube.position.z += 0.05;
+
+  var cubeB = new THREE.Box3().setFromObject( cube );
+  var roomB = new THREE.Box3().setFromObject( room );
+  var c = cube.position, cs = cubeB.getSize();
+  var r = room.position, rs = roomB.getSize();
+
+  if ( (c.x + cs.x/2) >= (r.x + rs.x/2) ) canMove.right = false;
+  else canMove.right = true;
+
+  if ( (c.x - cs.x/2) <= (r.x - rs.x/2) ) canMove.left = false;
+  else canMove.left = true;
+
+  if ( (c.y + cs.y/2) >= (r.y - rs.y/2) ) canMove.up = false;
+  else canMove.up = true;
+
 }
 
 function render() {
@@ -77,23 +108,23 @@ function loop() {
 
 function keyDown(event) {
   switch(event.which) {
-    case 38: keys.up = true; break;
-    case 40: keys.down = true; break;
-    case 37: keys.left = true; break;
-    case 39: keys.right = true; break;
-    case 87: keys.w = true; break;
-    case 83: keys.s = true; break;
+    case 69: keys.up = true; break;
+    case 81: keys.down = true; break;
+    case 65: keys.left = true; break;
+    case 68: keys.right = true; break;
+    case 87: keys.forward = true; break;
+    case 83: keys.backward = true; break;
   }
 }
 
 function keyUp(event) {
   switch(event.which) {
-    case 38: keys.up = false; break;
-    case 40: keys.down = false; break;
-    case 37: keys.left = false; break;
-    case 39: keys.right = false; break;
-    case 87: keys.w = false; break;
-    case 83: keys.s = false; break;
+    case 69: keys.up = false; break;
+    case 81: keys.down = false; break;
+    case 65: keys.left = false; break;
+    case 68: keys.right = false; break;
+    case 87: keys.forward = false; break;
+    case 83: keys.backward = false; break;
   }
 }
 
