9da84fdd34470ed5042197876704b15c8e73cb75
Plasmoxy <sebohacker@gmail.com>
2018-02-27 16:09:31 +0100

    nice game

--- dev/three.js/learning/helloworld/script.js
+++ dev/three.js/learning/helloworld/script.js
@@ -2,8 +2,11 @@ console.log('< BEGINNING script.js >');
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+
 
 window.addEventListener('resize', function() {
   var width = window.innerWidth;
@@ -13,8 +16,18 @@ window.addEventListener('resize', function() {
   camera.updateProjectionMatrix();
 });
 
+
 camera.position.z = 3;
 scene.background = new THREE.Color(0x111111);
+
+var keys = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+  w: false,
+  s: false
+};
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({
@@ -26,8 +39,12 @@ scene.add(cube);
 
 //render
 function update() {
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.01;
+  if (keys.up) cube.position.y += 0.05;
+  if (keys.down) cube.position.y -= 0.05;
+  if (keys.right) cube.position.x += 0.05;
+  if (keys.left) cube.position.x -= 0.05;
+  if (keys.w) cube.position.z -= 0.05;
+  if (keys.s) cube.position.z += 0.05;
 }
 
 function render() {
@@ -39,5 +56,30 @@ function loop() {
   update();
   render();
 }
+
+function keyDown(event) {
+  switch(event.which) {
+    case 38: keys.up = true; break;
+    case 40: keys.down = true; break;
+    case 37: keys.left = true; break;
+    case 39: keys.right = true; break;
+    case 87: keys.w = true; break;
+    case 83: keys.s = true; break;
+  }
+}
+
+function keyUp(event) {
+  switch(event.which) {
+    case 38: keys.up = false; break;
+    case 40: keys.down = false; break;
+    case 37: keys.left = false; break;
+    case 39: keys.right = false; break;
+    case 87: keys.w = false; break;
+    case 83: keys.s = false; break;
+  }
+}
+
+document.addEventListener('keydown', keyDown, false);
+document.addEventListener('keyup', keyUp, false);
 
 loop();
