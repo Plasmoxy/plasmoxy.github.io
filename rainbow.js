70708a1587d99edbd1d5c3b556bccb3ef4a1aaef
Plasmoxy <sebohacker@gmail.com>
2018-02-28 23:36:38 +0100

    WATCH THE NEW META XDD

--- rainbow.js
+++ rainbow.js
@@ -1,12 +1,17 @@
 console.log('starting rainbow.js')
-var rainbow_hue = 0;
+var rainbow_hue = 360;
 var titleContainer = document.getElementById("titleContainer");
 
+var newHue;
 setInterval(function() {
-  titleContainer.style.background = "hsl(" + rainbow_hue + ", 70%, 50%)";
-  if (rainbow_hue < 360) {
-    rainbow_hue++;
+  newHue =  (rainbow_hue+200)%360;
+  $('#titleContainer').css('background', "hsl(" + newHue + ", 70%, 50%)");
+  $('meta[name=theme-color]').attr('content', pmxyjs.hslToHex(newHue, 70, 50));
+
+  if (rainbow_hue > 0) {
+    rainbow_hue--;
   } else {
-    rainbow_hue = 0;
+    rainbow_hue = 360;
   }
+
 }, 100);
