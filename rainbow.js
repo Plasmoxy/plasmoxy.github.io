a1c83bdfa902ac0834c877eca1db030dd181515b
Plasmoxy <sebohacker@gmail.com>
2018-03-11 23:19:16 +0100

    rain

--- rainbow.js
+++ rainbow.js
@@ -4,7 +4,7 @@ var titleContainer = document.getElementById("titleContainer");
 
 var newHue;
 setInterval(function() {
-  newHue =  (rainbow_hue+200)%360;
+  newHue =  (rainbow_hue+50)%360;
   $('#titleContainer').css('background', "hsl(" + newHue + ", 70%, 50%)");
   $('meta[name=theme-color]').attr('content', pmxyjs.hslToHex(newHue, 70, 50));
 
