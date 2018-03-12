7f300f01b63b7357695eb72fe67ee1266f4e2404
Plasmoxy <sebohacker@gmail.com>
2018-03-12 22:36:55 +0100

    asd

--- loading.js
+++ loading.js
@@ -1,17 +1,17 @@
 
 (function() {
 
-  let pathway = new FontFaceObserver('Pathway Gothic One');
-  let ostrich = new FontFaceObserver('Ostrich Sans', {weight: 700});
+  setTimeout(function() {
+    let pathway = new FontFaceObserver('Pathway Gothic One');
+    let ostrich = new FontFaceObserver('Ostrich Sans', {weight: 700});
 
-  Promise.all([pathway.load(), ostrich.load()]).then(function () {
-    setTimeout(function() {
+    Promise.all([pathway.load(), ostrich.load()]).then(function () {
       console.log('[loading.js] Fonts loaded, enabling visibility')
       $('#loading_div').css('display', 'none');
       setTimeout(function() {
         $('#superroot').css('display', 'inline');
       }, 100);
-    }, 500);
-  });
+    });
+  }, 500);
 
 })();
