24a09ffd0f901540d5348c2c3af55c88be411a92
Plasmoxy <sebohacker@gmail.com>
2018-03-16 22:50:06 +0100

    pixibase

---
+++ dev/PixiJSRes/pixibase/main.js
@@ -0,0 +1,91 @@
+/* pixibase by Plasmoxy */
+/* uses my pixialiases.js snippet for shorter names */
+
+let fmeter = new FPSMeter();
+
+/* constants/aliases */
+const pi = Math.PI;
+
+/* setup pixi */
+let app = new Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  antialias: true,
+  transparent: false,
+  resolution: 1
+});
+
+app.view.style.display = "none";
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x111111;
+
+function loadProgressHandler(ldr, res) { // loader, resource
+  console.log('LOADING [ '+Math.round(ldr.progress)+'% ] : ' + res.name + ' -> ' + res.url);
+}
+
+/* define game variables */
+let bg, world, gui, camera, mkeys; // basic
+
+/* PIXI loader */
+loader
+  .on('progress', loadProgressHandler)
+  .load(setup)
+;
+
+function setup() {
+
+  /* hide loading and show app */
+  document.getElementById('loading').style.display = 'none';
+  app.view.style.display = "block";
+
+  /* INIT CONTAINERS - order is important ! */
+  world = new World(); app.stage.addChild(world);
+  gui = new Gui(); app.stage.addChild(gui);
+
+  // link camera to world
+  camera = new Camera(world);
+
+  /* define control */
+  mkeys = {
+    up: new KeyboardKey(38),
+    down: new KeyboardKey(40),
+    left: new KeyboardKey(37),
+    right: new KeyboardKey(39)
+  };
+
+
+  /* -- INIT GAME --- */
+
+
+
+  /* --- end INIT GAME ---*/
+
+  /* setup ticker */
+  app.ticker.add(tick);
+  /* start rendering */
+  update();
+}
+
+function update() {
+  requestAnimationFrame(update);
+  if ( fmeter ) fmeter.tick()
+}
+
+function tick(dt) {
+
+}
+
+// add some other listeners in the end
+
+window.addEventListener('resize', function() {
+  w = window.innerWidth;
+  h = window.innerHeight;
+
+  console.log(app.renderer.view.style.width)
+
+  app.renderer.view.style.width = w + "px";
+  app.renderer.view.style.height = h + "px";
+  app.renderer.resize(w,h);
+  camera.centerToRenderer();
+});
