1c2efd03c2b58882a134c5e6476b6a03761d94fa
Plasmoxy <sebohacker@gmail.com>
2018-04-02 22:28:37 +0200

    imag folower

--- dev/CodingTrain/p5/images/sketch.js
+++ dev/CodingTrain/p5/images/sketch.js
@@ -1,9 +1,9 @@
 /* uses p5.js | by Plasmoxy */
 
-
+let blyat;
 
 function preload() {
-
+  blyat = loadImage('/assets/img/cliccblyat.png');
 }
 
 function setup() {
@@ -11,5 +11,6 @@ function setup() {
 }
 
 function draw() {
-
+  clear();
+  image(blyat, mouseX-200, mouseY-150, 400, 300);
 }
