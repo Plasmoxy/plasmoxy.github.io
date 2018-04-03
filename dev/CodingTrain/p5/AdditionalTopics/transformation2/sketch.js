e3628aa5c632da900687d74907f29d6ffaf486d8
Plasmoxy <sebohacker@gmail.com>
2018-04-03 18:19:29 +0200

    t2

--- dev/CodingTrain/p5/AdditionalTopics/transformation2/sketch.js
+++ dev/CodingTrain/p5/AdditionalTopics/transformation2/sketch.js
@@ -2,6 +2,9 @@
 
 let canvas, bgColor;
 
+let a = 0, i = 0.05;
+
+
 function preload() {
   
 }
@@ -18,6 +21,18 @@ function setup() {
 
 function draw() {
   background(bgColor);
+  fill(255);
+  
+  push();
+  rectMode(CENTER);
+  translate(canvas.width/2, canvas.height/2);
+  scale(a);
+  rect(0, 0, 50, 50);
+  pop();
+  
+  if (a > 3) i = -0.05;
+  if (a < 0) i = 0.05;
+  a += i;
   
 }
 
