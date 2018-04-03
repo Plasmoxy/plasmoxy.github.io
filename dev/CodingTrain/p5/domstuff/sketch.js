5767c96de3dd76892234986e2192b413b8f2cacd
Plasmoxy <sebohacker@gmail.com>
2018-04-03 13:09:13 +0200

    no need

--- dev/CodingTrain/p5/domstuff/sketch.js
+++ dev/CodingTrain/p5/domstuff/sketch.js
@@ -1,16 +1,17 @@
 /* uses p5.js | by Plasmoxy */
 
 let canvas, btn, slider;
+let bgColor = color()
 
 function preload() {
   
 }
 
 function setup() {
-  canvas = createCanvas(600, 400);
+  canvas = createCanvas(600, 300);
   createP('');
   
-  btn = createButton('XD');
+  btn = createButton('hello');
   btn.mousePressed(btnPressed);
   
   slider = createSlider(10, 100, 47);
