396cea80b188f20dee1e432da1eacbf7ad919b61
Plasmoxy <sebohacker@gmail.com>
2018-04-03 19:30:36 +0200

    closure

--- dev/CodingTrain/p5/AdditionalTopics/closure1/sketch.js
+++ dev/CodingTrain/p5/AdditionalTopics/closure1/sketch.js
@@ -1,6 +1,7 @@
 /* uses p5.js | by Plasmoxy */
 
 let canvas, bgColor;
+let timerp, timerp2;
 
 function preload() {
   
@@ -12,8 +13,29 @@ function setup() {
   canvas.parent('canvasroot');
   canvas.mousePressed(canvasPressed);
   
+  timerp = createP('');
+  timerp2 = createP('');
+  
+  makeTimer(timerp2, 1000);
+  makeTimer(timerp, 500);
   
   
+  // console.log(counter); // should output undefined
+  
+}
+
+// this is a CLOSURE
+function makeTimer(elem, wait) {
+  let counter = 0; // counter for this function
+  
+  function timeIt() {
+    elem.html(counter);
+    counter++;
+  }
+  
+  // even after the function finishes, the counter variable
+  // will still be alive as setInterval NEEDS it
+  return setInterval(timeIt, wait);
 }
 
 function draw() {
