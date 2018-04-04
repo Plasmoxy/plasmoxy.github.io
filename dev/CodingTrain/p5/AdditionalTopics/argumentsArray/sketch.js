5e13b0b0333ad3bea8cd6cfbc8b187de027e5867
Plasmoxy <sebohacker@gmail.com>
2018-04-04 23:04:55 +0200

    particle

--- dev/CodingTrain/p5/AdditionalTopics/argumentsArray/sketch.js
+++ dev/CodingTrain/p5/AdditionalTopics/argumentsArray/sketch.js
@@ -1,6 +1,25 @@
 /* uses p5.js | by Plasmoxy */
 
-let canvas, bgColor;
+class Particle {
+  constructor() {
+    for (let arg of arguments) {
+      if (arguments.length == 1 && arg instanceof p5.Vector) {
+        this.pos = arg;
+      }
+    }
+    
+    if (!arguments.length) this.pos = createVector(width/2, height/2);
+    
+  }
+  
+  draw() {
+    push();
+    fill(255, 0, 255);
+    translate(this.pos.x, this.pos.y);
+    ellipse(0, 0, 16, 16);
+    pop();
+  }
+}
 
 // arguments keyword variable of a JS function contains its arguments
 function sum() {
@@ -16,20 +35,27 @@ function sum() {
     }
   }
   
-  
-  
   return val;
 }
+
+let canvas, bgColor;
+
+let particles = [];
 
 function setup() {
   bgColor = color(10);
   canvas = createCanvas(windowWidth, windowHeight);
   
-  
+  particles.push(
+    new Particle(createVector(30,30)),
+    new Particle()
+  );
   
 }
 
 function draw() {
   background(bgColor);
-  
+  for (let p of particles) {
+    p.draw();
+  }
 }
