aeb7282e7c717d408a25aa5a63a8d17c28f2cce3
Plasmoxy <sebohacker@gmail.com>
2018-04-04 23:17:48 +0200

    polar

---
+++ dev/CodingTrain/p5/AdditionalTopics/polarCoordinates/sketch.js
@@ -0,0 +1,16 @@
+/* uses p5.js | by Plasmoxy */
+
+let canvas, bgColor;
+
+function setup() {
+  bgColor = color(10);
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+  
+  
+}
+
+function draw() {
+  background(bgColor);
+  
+}
