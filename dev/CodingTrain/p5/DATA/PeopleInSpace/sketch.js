b21c6ea9328400a63e49ba59935f1f1893e5f328
Plasmoxy <sebohacker@gmail.com>
2018-04-07 21:01:29 +0200

    space

---
+++ dev/CodingTrain/p5/DATA/PeopleInSpace/sketch.js
@@ -0,0 +1,20 @@
+/* uses p5.js | by Plasmoxy | @template=hybrid */
+
+
+function setup() {
+  let c = createCanvas(200, 200);
+  loadJSON('http://api.open-notify.org/astros.json', gotData);
+}
+
+function gotData(data) {
+  createElement('h1', 'People in space : ');
+  for (let i = 0; i < data.number; i++) {
+    let person = data.people[i];
+
+    fill(255);
+    ellipse(random(width), random(height), 16, 16);
+    createDiv(person.name + ' - ' + person.craft);
+  }
+}
+
+function draw() {}
