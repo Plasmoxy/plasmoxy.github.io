80be5e234ed80ec0265de03664929a9a1dc187cd
Plasmoxy <sebohacker@gmail.com>
2018-04-07 22:05:29 +0200

    weather

---
+++ dev/CodingTrain/p5/DATA/Weather/sketch.js
@@ -0,0 +1,27 @@
+/* uses p5.js | by Plasmoxy | @template=hybrid */
+
+let temp;
+
+function setup() {
+  noCanvas();
+  select('#cityinput').input(loadData);
+}
+
+function getWeatherData(city, callback) {
+  loadJSON(
+    'http://api.openweathermap.org/data/2.5/weather?q='
+    +city
+    +'&appid=829d5a1402e3d3406b70ef9c8028f73f&units=metric'
+  , callback);
+}
+
+function loadData() {
+  getWeatherData(select('#cityinput').value(), gotData);
+}
+
+function gotData(data) {
+  temp = Number(data.main.temp);
+  select('#info').html('Temperature = ' + temp + ' °C');
+}
+
+function draw() {}
