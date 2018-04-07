bfb6f5384a22706fd9429b01c7961b17c17c5332
Plasmoxy <sebohacker@gmail.com>
2018-04-07 22:15:31 +0200

    fix

--- dev/CodingTrain/p5/DATA/Weather/sketch.js
+++ dev/CodingTrain/p5/DATA/Weather/sketch.js
@@ -7,21 +7,26 @@ function setup() {
   select('#cityinput').input(loadData);
 }
 
-function getWeatherData(city, callback) {
+function getWeatherData(city, callback, errorcallback) {
   loadJSON(
     'https://api.openweathermap.org/data/2.5/weather?q='
     +city
     +'&appid=829d5a1402e3d3406b70ef9c8028f73f&units=metric'
-  , callback);
+  , callback, errorcallback);
 }
 
 function loadData() {
-  getWeatherData(select('#cityinput').value(), gotData);
+  getWeatherData(select('#cityinput').value(), gotData, loadError);
 }
 
 function gotData(data) {
+  console.log(data);
   temp = Number(data.main.temp);
   select('#info').html('Temperature = ' + temp + ' °C');
+}
+
+function loadError() {
+  select('#info').html('No such city.');
 }
 
 function draw() {}
