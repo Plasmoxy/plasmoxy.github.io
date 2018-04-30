4134c72e155ded8efae0d0bfad52db433ad08eaa
Plasmoxy <sebohacker@gmail.com>
2018-05-01 00:01:22 +0200

    ayy

---
+++ dev/CodingTrain/PerlinNoise/2DPerlin/sketch.js
@@ -0,0 +1,16 @@
+/* uses p5.js | by Plasmoxy | @template=fullscreen */
+
+let canvas, bgColor;
+
+function setup() {
+  bgColor = color(10);
+  canvas = createCanvas(windowWidth, windowHeight);
+
+
+
+}
+
+function draw() {
+  background(bgColor);
+
+}
