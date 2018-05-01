86ae73281528acd9608f56c4b833adcda1d3d502
Plasmoxy <sebohacker@gmail.com>
2018-05-01 12:20:58 +0200

    perlin

--- dev/CodingTrain/PerlinNoise/2DPerlin/sketch.js
+++ dev/CodingTrain/PerlinNoise/2DPerlin/sketch.js
@@ -2,15 +2,33 @@
 
 let canvas, bgColor;
 
+let inc = 0.01;
+
+det = 0;
+
 function setup() {
   bgColor = color(10);
   canvas = createCanvas(windowWidth, windowHeight);
-
-
-
+  noiseDetail(12, 0.4);
 }
 
 function draw() {
-  background(bgColor);
 
+  let yoff = 0;
+
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    let xoff = 0;
+    for (let y = 0; y < height; y++) {
+      let i = (x + y*width)*4;
+      let r = 255*noise(xoff, yoff);
+      pixels[i + 0] = r;
+      pixels[i + 1] = r;
+      pixels[i + 2] = r;
+      pixels[i + 3] = 255;
+      xoff += inc;
+    }
+    yoff += inc;
+  }
+  updatePixels();
 }
